f4047760e0394171cc0442eb4c3264f5cb7a3d2e
Adrian Kirsten <adrian@wealthpoint.app>
2023-07-13 10:19:52 +0200

    feature: add error handlers

--- server/src/core/services/api/api.schema.ts
+++ server/src/core/services/api/api.schema.ts
@@ -11,6 +11,21 @@ export enum ApiMethodType {
   Patch = 'patch',
 }
 
+export enum ApiStatusCode {
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  FORBIDDEN = 403,
+  UNAUTHORIZED = 401,
+  METHOD_NOT_ALLOWED = 405,
+  REQUEST_TIMEOUT = 408,
+  CONFLICT = 409,
+  INTERNAL_SERVER = 500,
+}
+
 // SCHEMA: INTERFACES
 
 export interface ApiServiceInterface {
