6de732d21b28cf9bc51f85f2d82c2d3c1f81eac6
Adrian Kirsten <adrian@wealthpoint.app>
2023-07-28 09:30:28 +0200

    killall

--- README.md
+++
@@ -1,3 +0,0 @@
-# Workbench
-
-This repository represents a workbench where I can test out and build different things. I don't expect it to be useful to anyone but me.
--- backend/src/modules/index.ts
+++
@@ -1,9 +0,0 @@
-import { Provider } from '@/app/tools/ioc';
-
-import { userProviders } from './user';
-
-const moduleProviders: Provider[] = [
-  ...userProviders,
-];
-
-export default moduleProviders;
